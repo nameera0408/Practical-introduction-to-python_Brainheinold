b1b6de0c521d2c28d9880c874571712df1af27a4
nameera0408 <92710149+nameera0408@users.noreply.github.com>
2021-10-18 20:06:27 +0530

    Create ch3_10sol.py

---
+++ ch3_10sol.py
@@ -0,0 +1,10 @@
+#1.
+number = eval(input("Enter the Value of the Exponent: "))
+
+find_a = 2**number%10
+print("The Last two digits of the " , find_a)
+#2
+number = eval(input("Enter the Value of the Exponent: "))
+
+find_a = 2**number%100
+print("The Last two digits of the " , find_a)
