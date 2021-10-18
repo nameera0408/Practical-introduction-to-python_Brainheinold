e41a06e4a0281d38b616558ddd8607a246d0a88a
nameera0408 <92710149+nameera0408@users.noreply.github.com>
2021-10-18 23:22:30 +0530

    Create ch4_7sol.py

---
+++ ch4_7sol.py
@@ -0,0 +1,13 @@
+n1 = eval(input("Enter First Number: "))
+n2 = eval(input("Enter Second Number:"))
+
+if n1 + 0.001 == n2:
+
+     print("close")
+
+elif n2 + 0.001 == n1:
+
+     print("close")
+
+else:
+     print("Not close")
