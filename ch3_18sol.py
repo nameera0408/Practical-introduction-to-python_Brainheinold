86aeb561d789dfedcc9f378d1b0a67c8d5b164ba
nameera0408 <92710149+nameera0408@users.noreply.github.com>
2021-10-18 20:36:59 +0530

    Create ch3_18sol.py

---
+++ ch3_18sol.py
@@ -0,0 +1,7 @@
+number=int(input())
+Quater = number // 25
+Dimes = number // 10
+Nickel = number // 5
+Penny = number // 1
+
+print("Quater = ",Quater, "Dimes = ", Dimes, "Nickel = ", Nickel, "Penny = ", Penny)
