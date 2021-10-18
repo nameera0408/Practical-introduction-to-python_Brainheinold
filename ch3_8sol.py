b6467dee28a22f783b6ebec8153fb537d5f7d82e
nameera0408 <92710149+nameera0408@users.noreply.github.com>
2021-10-18 18:01:27 +0530

    Create ch3_8sol.py

---
+++ ch3_8sol.py
@@ -0,0 +1,7 @@
+seconds= eval(input("Enter the number of seconds: "))
+
+
+time_in_minutes =int(seconds /60)
+time_in_seconds = seconds % 60
+
+print("The  time is " , time_in_minutes,"minutes ", time_in_seconds,"seconds")
