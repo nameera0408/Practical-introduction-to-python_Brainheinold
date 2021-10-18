46496a6d0cb81ac3225e2674db3536e025a594b5
nameera0408 <92710149+nameera0408@users.noreply.github.com>
2021-10-18 20:07:51 +0530

    Create ch3_11sol.py

---
+++ ch3_11sol.py
@@ -0,0 +1,6 @@
+kg = eval(input("Enter Your Weights in Kg: "))
+
+pound = round(weight_kg * 2.20,1)
+
+print("Your Weight in Pounds is :",pound)
+#1/10 means pounds having only one number after the decimal point
